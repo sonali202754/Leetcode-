7e5a3fc5865b3b8f6eb42aa9227cba57960cb478
sonalipaul <sonalipaul461@gmail.com>
2026-07-09 11:15:15 +0530

    Time: 3 ms (65.72%), Space: 7.8 MB (37.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -8,13 +8,19 @@ public:
     // }
     int fib(int n) {
         if(n<=1)return n;
-        vector<int>dp(n+1,-1);
+        if(n==2)return 1;
+        vector<int>dp(3);
         //base case fill in bottom up approch 
         dp[0]=0;
         dp[1]=1;
-        for(int i=2; i<=n; i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        dp[2]=1;
+        for(int i=3; i<=n; i++){
+            // spcae optimization
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+            dp[2]=dp[0]+dp[1];
+           
         }
-         return dp[n];
+         return dp[2];
     }
 };
