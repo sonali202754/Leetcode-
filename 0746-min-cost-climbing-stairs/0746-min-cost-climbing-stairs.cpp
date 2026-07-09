7cec49fb4b121bdb27057334ac29de9f45aa6010
sonalipaul <sonalipaul461@gmail.com>
2026-07-09 12:44:21 +0530

    Time: 0 ms (100%), Space: 17.5 MB (73.96%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -7,13 +7,19 @@ public:
 //    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int>dp(n+1,-1);
-        // use bottom up approch
+        // vector<int>dp(n+1,-1); // use bottom up approch
+       
+        vector<int>dp(3);
         dp[0]=0;
         dp[1]=0;
-        for(int i=2; i<=n;i++){
-            dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        dp[2]=min(cost[0],cost[1]);
+        for(int i=3; i<=n;i++){
+            // dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+            //space optimization
+           dp[0]=dp[1];
+           dp[1]=dp[2];
+           dp[2]=min(dp[0]+cost[i-2],dp[1]+cost[i-1]);
         }
-        return dp[n];
+        return dp[2];
     }
 };
