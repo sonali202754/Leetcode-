7ac030921fb07c30b6f433dc1e76fb5402393b90
sonalipaul <sonalipaul461@gmail.com>
2026-07-09 14:12:54 +0530

    Time: 0 ms (100%), Space: 10.2 MB (71.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,13 +11,17 @@ public:
         int n= nums.size();
         if(n==1)return nums[0];
         if(n==2)return max(nums[0],nums[1]);
-        vector<int>dp(n,-1);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        vector<int>dp(3,0);
+        dp[1]=nums[0];
+        dp[2]=max(nums[0],nums[1]);
         for(int i=2; i<n;i++){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            // dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            //space optimization
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+            dp[2]=max(nums[i]+dp[0],dp[1]);
         }
     
-        return dp[n-1];
+        return dp[2];
     }
 };
