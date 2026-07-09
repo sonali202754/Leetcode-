cd9b8e826e9bce5cc81c0524bc8e667fe1feb7f0
sonalipaul <sonalipaul461@gmail.com>
2026-07-09 11:44:27 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -15,8 +15,12 @@ public:
         dp[1]=1;
         dp[2]=2;
         for(int i=3; i<=n; i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            //space optimization
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+            dp[2]=dp[0]+dp[1];
+            
         }
-       return dp[n];
+       return dp[2];
     }
 };
